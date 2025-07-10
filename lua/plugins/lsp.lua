e7447f7c487f7a1c7cb212fef41f6eca91cac76f
flashfishgit <102473074+flashfishgit@users.noreply.github.com>
2025-07-10 12:30:23 +0200

    modify lsp for espidf without plugin

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -3,35 +3,19 @@ return {
   opts = function(_, opts)
     local lspconfig = require("lspconfig")
     local util = require("lspconfig.util")
-    local esp32 = require("esp32")
 
-    local root = require("lazyvim.util").root.get()
-
-    local function is_esp_project(root_dir)
-      return vim.fn.filereadable(root_dir .. "/.esp") == 1
-    end
-
-    if is_esp_project(root) then
-      print("ESP32 project detected, setting up ESP32 LSP...")
-      opts.servers = opts.servers or {}
-      opts.servers.clangd = esp32.lsp_config()
-
-      --[[
-      opts.cmd = {
-        "/home/flashfish/.espressif/tools/esp-clang/esp-18.1.2_20240912/esp-clang/bin/clangd",
-        "--compile-commands-dir=build.clang",
+    opts.servers = opts.servers or {}
+    opts.servers.clangd = {
+      cmd = {
+        "clangd",
         "--background-index",
-        "--clang-tidy",
-        "--header-insertion=iwyu",
-        "--completion-style=detailed",
-        "--function-arg-placeholders",
-        "--fallback-style=llvm",
-      }
-      ]]
-    else
-      print("No ESP32 project detected, using default clangd setup...")
-      opts.cmd = { "clangd" } -- Use system default
-    end
+        "--query-driver=/home/flashfish/.espressif/tools/xtensa-esp-elf/esp-14.2.0_20241119/xtensa-esp-elf/bin/*-gcc",
+      },
+      root_dir = lspconfig.util.root_pattern("build/compile_commands.json", ".git"),
+    }
+
+    -- debug print so you can see exactly what Neovim will launch:
+    print("🚀 clangd command:", vim.inspect(opts.servers.clangd.cmd))
 
     lspconfig.clangd.setup(opts)
     return opts
