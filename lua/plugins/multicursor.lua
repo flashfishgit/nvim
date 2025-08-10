29622d0dc1a19a608eb7bee868bd8a836458be0f
flashfishgit <102473074+flashfishgit@users.noreply.github.com>
2025-08-10 16:57:40 +0200

    Fix Multicursor, and highlighting

--- lua/plugins/multicursor.lua
+++ lua/plugins/multicursor.lua
@@ -1,77 +1,106 @@
 return {
-  'jake-stewart/multicursor.nvim',
-  branch = '1.0',
-  config = function()
-    local mc = require 'multicursor-nvim'
-    mc.setup()
+  {
+    "LazyVim/LazyVim",
+    init = function()
+      -- run *after* LazyVim sets its defaults
+      vim.api.nvim_create_autocmd("User", {
+        pattern = "LazyVimKeymaps",
+        callback = function()
+          for _, mode in ipairs({ "n", "x" }) do
+            pcall(vim.keymap.del, mode, "<Up>")
+            pcall(vim.keymap.del, mode, "<Down>")
+          end
 
-    local set = vim.keymap.set
+          -- now your multicursor bindings
+          local mc = require("multicursor-nvim")
+          local set = vim.keymap.set
+          set({ "n", "x" }, "<Up>", function()
+            mc.lineAddCursor(-1)
+          end, { noremap = true, silent = true })
+          set({ "n", "x" }, "<Down>", function()
+            mc.lineAddCursor(1)
+          end, { noremap = true, silent = true })
+          set({ "n", "x" }, "<leader><Up>", function()
+            mc.lineSkipCursor(-1)
+          end, { noremap = true, silent = true })
+          set({ "n", "x" }, "<leader><Down>", function()
+            mc.lineSkipCursor(1)
+          end, { noremap = true, silent = true })
+        end,
+      })
+    end,
+  },
 
-    -- Add or skip cursor above/below the main cursor.
-    set({ 'n', 'x' }, '<up>', function()
-      mc.lineAddCursor(-1)
-    end)
-    set({ 'n', 'x' }, '<down>', function()
-      mc.lineAddCursor(1)
-    end)
-    set({ 'n', 'x' }, '<leader><up>', function()
-      mc.lineSkipCursor(-1)
-    end)
-    set({ 'n', 'x' }, '<leader><down>', function()
-      mc.lineSkipCursor(1)
-    end)
+  {
+    "jake-stewart/multicursor.nvim",
+    branch = "1.0",
+    lazy = false,
+    config = function()
+      local mc = require("multicursor-nvim")
+      mc.setup()
 
-    -- Add or skip adding a new cursor by matching word/selection
-    set({ 'n', 'x' }, '<leader>n', function()
-      mc.matchAddCursor(1)
-    end)
-    set({ 'n', 'x' }, '<leader>s', function()
-      mc.matchSkipCursor(1)
-    end)
-    set({ 'n', 'x' }, '<leader>N', function()
-      mc.matchAddCursor(-1)
-    end)
-    set({ 'n', 'x' }, '<leader>S', function()
-      mc.matchSkipCursor(-1)
-    end)
+      local set = vim.keymap.set
 
-    -- Add and remove cursors with control + left click.
-    set('n', '<c-leftmouse>', mc.handleMouse)
-    set('n', '<c-leftdrag>', mc.handleMouseDrag)
-    set('n', '<c-leftrelease>', mc.handleMouseRelease)
-
-    -- Disable and enable cursors.
-    set({ 'n', 'x' }, '<c-q>', mc.toggleCursor)
-
-    -- Mappings defined in a keymap layer only apply when there are
-    -- multiple cursors. This lets you have overlapping mappings.
-    mc.addKeymapLayer(function(layerSet)
-      -- Select a different cursor as the main one.
-      layerSet({ 'n', 'x' }, '<left>', mc.prevCursor)
-      layerSet({ 'n', 'x' }, '<right>', mc.nextCursor)
-
-      -- Delete the main cursor.
-      layerSet({ 'n', 'x' }, '<leader>x', mc.deleteCursor)
-
-      -- Enable and clear cursors using escape.
-      layerSet('n', 'df', function()
-        if not mc.cursorsEnabled() then
-          mc.enableCursors()
-        else
-          mc.clearCursors()
-        end
-        set({ 'n', 'x' }, '<leader>n', mc.matchAddCursor)
+      -- Add or skip cursor above/below the main cursor.
+      set({ "n", "x" }, "<Up>", function()
+        mc.lineAddCursor(-1)
       end)
-    end)
+      set({ "n", "x" }, "<Down>", function()
+        mc.lineAddCursor(1)
+      end)
+      set({ "n", "x" }, "<leader><Up>", function()
+        mc.lineSkipCursor(-1)
+      end)
+      set({ "n", "x" }, "<leader><Down>", function()
+        mc.lineSkipCursor(1)
+      end)
 
-    -- Customize how cursors look.
-    local hl = vim.api.nvim_set_hl
-    hl(0, 'MultiCursorCursor', { reverse = true })
-    hl(0, 'MultiCursorVisual', { link = 'Visual' })
-    hl(0, 'MultiCursorSign', { link = 'SignColumn' })
-    hl(0, 'MultiCursorMatchPreview', { link = 'Search' })
-    hl(0, 'MultiCursorDisabledCursor', { reverse = true })
-    hl(0, 'MultiCursorDisabledVisual', { link = 'Visual' })
-    hl(0, 'MultiCursorDisabledSign', { link = 'SignColumn' })
-  end,
+      -- Match-based add/skip
+      set({ "n", "x" }, "<leader>n", function()
+        mc.matchAddCursor(1)
+      end)
+      set({ "n", "x" }, "<leader>s", function()
+        mc.matchSkipCursor(1)
+      end)
+      set({ "n", "x" }, "<leader>N", function()
+        mc.matchAddCursor(-1)
+      end)
+      set({ "n", "x" }, "<leader>S", function()
+        mc.matchSkipCursor(-1)
+      end)
+
+      -- Mouse
+      set("n", "<C-LeftMouse>", mc.handleMouse)
+      set("n", "<C-LeftDrag>", mc.handleMouseDrag)
+      set("n", "<C-LeftRelease>", mc.handleMouseRelease)
+
+      -- Toggle
+      set({ "n", "x" }, "<C-q>", mc.toggleCursor)
+
+      -- Layer (active only when multiple cursors exist)
+      mc.addKeymapLayer(function(layerSet)
+        layerSet({ "n", "x" }, "<Left>", mc.prevCursor)
+        layerSet({ "n", "x" }, "<Right>", mc.nextCursor)
+        layerSet({ "n", "x" }, "<leader>x", mc.deleteCursor)
+        layerSet("n", "df", function()
+          if not mc.cursorsEnabled() then
+            mc.enableCursors()
+          else
+            mc.clearCursors()
+          end
+          set({ "n", "x" }, "<leader>n", mc.matchAddCursor)
+        end)
+      end)
+
+      -- Highlights
+      local hl = vim.api.nvim_set_hl
+      hl(0, "MultiCursorCursor", { reverse = true })
+      hl(0, "MultiCursorVisual", { link = "Visual" })
+      hl(0, "MultiCursorSign", { link = "SignColumn" })
+      hl(0, "MultiCursorMatchPreview", { link = "Search" })
+      hl(0, "MultiCursorDisabledCursor", { reverse = true })
+      hl(0, "MultiCursorDisabledVisual", { link = "Visual" })
+      hl(0, "MultiCursorDisabledSign", { link = "SignColumn" })
+    end,
+  },
 }
