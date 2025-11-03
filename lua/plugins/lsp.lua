686ed7f743ca8afa877f2ac4559a57616454881a
flashfishgit <102473074+flashfishgit@users.noreply.github.com>
2025-11-04 00:52:41 +0100

    s

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,49 +1,50 @@
 return {
   "neovim/nvim-lspconfig",
-  opts = function(_, opts)
-    --local lspconfig = require("lspconfig")
-    local util = require("lspconfig.util")
-
-    opts.servers = opts.servers or {}
-    opts.servers.clangd = {
-      cmd = {
-        "clangd",
-        "--background-index",
-        "--completion-style=detailed",
-        "--clang-tidy",
-        --        "--clang-tidy-checks=*",
-        "--function-arg-placeholders",
-        "--header-insertion=iwyu",
-        -- "--query-driver=/home/flashfish/.espressif/tools/xtensa-esp-elf/esp-14.2.0_20241119/xtensa-esp-elf/bin/*-gcc",
+  opts = {
+    servers = {
+      -- Ensure mason installs the server
+      clangd = {
+        keys = {
+          { "<leader>ch", "<cmd>LspClangdSwitchSourceHeader<cr>", desc = "Switch Source/Header (C/C++)" },
+        },
+        root_markers = {
+          "compile_commands.json",
+          "compile_flags.txt",
+          "configure.ac", -- AutoTools
+          "Makefile",
+          "configure.ac",
+          "configure.in",
+          "config.h.in",
+          "meson.build",
+          "meson_options.txt",
+          "build.ninja",
+          ".git",
+        },
+        capabilities = {
+          offsetEncoding = { "utf-16" },
+        },
+        cmd = {
+          "clangd",
+          "--background-index",
+          "--clang-tidy",
+          "--header-insertion=iwyu",
+          "--completion-style=detailed",
+          "--function-arg-placeholders",
+          "--fallback-style=llvm",
+        },
+        init_options = {
+          usePlaceholders = true,
+          completeUnimported = true,
+          clangdFileStatus = true,
+        },
       },
-      capabilities = {
-        offsetEncoding = { "utf-16" },
-      },
-      root_markers = {
-        "compile_commands.json",
-        "compile_flags.txt",
-        "configure.ac", -- AutoTools
-        "Makefile",
-        "configure.ac",
-        "configure.in",
-        "config.h.in",
-        "meson.build",
-        "meson_options.txt",
-        "build.ninja",
-        ".git",
-      },
-      init_options = {
-        usePlaceholders = true,
-        completeUnimported = true,
-        clangdFileStatus = true,
-      },
-      -- root_dir = lspconfig.util.root_pattern("build/compile_commands.json", ".git"),
-    }
-
-    -- debug print so you can see exactly what Neovim will launch:
-    print("🚀 clangd command:", vim.inspect(opts.servers.clangd.cmd))
-
-    -- lspconfig.clangd.setup(opts)
-    return opts
-  end,
+    },
+    setup = {
+      clangd = function(_, opts)
+        local clangd_ext_opts = LazyVim.opts("clangd_extensions.nvim")
+        require("clangd_extensions").setup(vim.tbl_deep_extend("force", clangd_ext_opts or {}, { server = opts }))
+        return false
+      end,
+    },
+  },
 }
